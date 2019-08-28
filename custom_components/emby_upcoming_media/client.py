49b2c85f8a145ee5ac20ee7900bb0019931bc7e3
Guillaume Corgnet <gcorgnet@gmail.com>
2019-08-28 19:26:14 +1200

    Include Rating in Emby Request

--- custom_components/emby_upcoming_media/client.py
+++ custom_components/emby_upcoming_media/client.py
@@ -42,7 +42,7 @@ class EmbyClient:
 
     def get_data(self, categoryId):
         try:
-            url = "http{0}://{1}:{2}/Users/{3}/Items/Latest?Limit={4}&ParentId={5}&api_key={6}".format(
+            url = "http{0}://{1}:{2}/Users/{3}/Items/Latest?Limit={4}&Fields=CommunityRating&ParentId={5}&api_key={6}".format(
                 self.ssl,
                 self.host,
                 self.port,
